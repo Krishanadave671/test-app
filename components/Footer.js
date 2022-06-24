af8cf8ba122a6ba81b82303df97e2623b44c8bc1
krishana dave <krishnadave671@gmail.com>
2022-06-24 15:43:48 +0530

    login page completed

---
+++ components/Footer.js
@@ -0,0 +1,164 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer ClassName="text-gray-600 body-font">
+  <div ClassName="container px-5 py-24 mx-auto">
+    <div ClassName="flex flex-wrap md:text-left text-center -mb-10 -mx-4">
+      <div ClassName="lg:w-1/6 md:w-1/2 w-full px-4">
+        <h2 ClassName="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav ClassName="list-none mb-10">
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div ClassName="lg:w-1/6 md:w-1/2 w-full px-4">
+        <h2 ClassName="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav ClassName="list-none mb-10">
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div ClassName="lg:w-1/6 md:w-1/2 w-full px-4">
+        <h2 ClassName="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav ClassName="list-none mb-10">
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div ClassName="lg:w-1/6 md:w-1/2 w-full px-4">
+        <h2 ClassName="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav ClassName="list-none mb-10">
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div ClassName="lg:w-1/6 md:w-1/2 w-full px-4">
+        <h2 ClassName="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav ClassName="list-none mb-10">
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div ClassName="lg:w-1/6 md:w-1/2 w-full px-4">
+        <h2 ClassName="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav ClassName="list-none mb-10">
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a ClassName="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div ClassName="border-t border-gray-200">
+    <div ClassName="container px-5 py-8 flex flex-wrap mx-auto items-center">
+      <div ClassName="flex md:flex-nowrap flex-wrap justify-center items-end md:justify-start">
+        <div ClassName="relative sm:w-64 w-40 sm:mr-4 mr-2">
+          <label for="footer-field" ClassName="leading-7 text-sm text-gray-600">Placeholder</label>
+          <input type="text" id="footer-field" name="footer-field" ClassName="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        </div>
+        <button ClassName="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+        <p ClassName="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones fanny pack
+           <br />
+        </p>
+      </div>
+      <span ClassName="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
+        <a ClassName="text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" ClassName="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a ClassName="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" ClassName="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+          </svg>
+        </a>
+        <a ClassName="ml-3 text-gray-500">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" ClassName="w-5 h-5" viewBox="0 0 24 24">
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a ClassName="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" ClassName="w-5 h-5" viewBox="0 0 24 24">
+            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+      </span>
+    </div>
+  </div>
+  <div ClassName="bg-gray-100">
+    <div ClassName="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <p ClassName="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
+        <a href="https://twitter.com/knyttneve" ClassName="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@knyttneve</a>
+      </p>
+      <span ClassName="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enamel pin tousled raclette tacos irony</span>
+    </div>
+  </div>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
